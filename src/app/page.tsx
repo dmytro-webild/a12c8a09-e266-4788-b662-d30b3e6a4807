7b5314d50b2caf6e3fee3bf6e8685a5ba6583830
bender <kudin.dima@gmail.com>
2026-05-11 19:30:07 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function LandingPage() {
       <HeroSplit
       background={{ variant: "gradient-bars" }}
       title="Lazar Uleksin"
-      description="Active, hardworking, and dedicated. Passionate about dance, sports, and delivering exceptional customer service."
+      description="&quot;Discipline is doing it even when you don't feel like it.&quot;"
       imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778527447087-zxgb57ao.jpg"
       imageAlt="Lazar Uleksin headshot"
       mediaAnimation="slide-up"
