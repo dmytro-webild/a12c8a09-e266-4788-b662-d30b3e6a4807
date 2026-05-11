5a1010f72c1510440d5ca193eb21041877c5389c
bender <kudin.dima@gmail.com>
2026-05-11 20:19:43 +0000

    Switch to version 27: modified src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,7 @@ export default function LandingPage() {
       background={{ variant: "radial-gradient" }}
       title="Lazar Uleksin"
       description="Distsipliin on tegemine isegi siis, kui sa seda ei tunne."
+      buttons={[{ text: "Tutvu projektidega", href: "#projects" }, { text: "Võta ühendust", href: "#contact" }]}
       imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778527447087-zxgb57ao.jpg"
       imageAlt="Lazar Uleksin"
       mediaAnimation="blur-reveal"
