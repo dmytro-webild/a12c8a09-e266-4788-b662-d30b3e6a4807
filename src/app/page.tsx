b0931713a2c0df13b2f99095d6e47528f34d8971
bender <kudin.dima@gmail.com>
2026-05-13 16:05:14 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ContactSplit from '@/components/sections/contact/ContactSplit';
 import FeatureCardMedia from '@/components/sections/feature/FeatureCardMedia';
 import FooterBaseReveal from '@/components/sections/footer/FooterBaseReveal';
 import HeroSplit from '@/components/sections/hero/HeroSplit';
-import MetricSplitMediaAbout from '@/components/sections/about/MetricSplitMediaAbout';
+import SplitAbout from '@/components/sections/about/SplitAbout';
 import NavbarStyleFullscreen from '@/components/navbar/NavbarStyleFullscreen/NavbarStyleFullscreen';
 
 export default function LandingPage() {
@@ -49,18 +49,13 @@ export default function LandingPage() {
   </div>
 
   <div id="about" data-section="about">
-      <MetricSplitMediaAbout
-      useInvertedBackground={false}
+      <SplitAbout
       title="Minust ja minu teekonnast"
-      description="Olen 18-aastane Tallinnast, kes on leidnud oma kirg tantsu, jalgpalli ja fotograafia kaudu. Reisimine on avanud mulle maailma"
-      metrics={[
-        { value: "18 ", title: "Minu vanus" },
-        { value: "Tallinn ", title: "Asukoht " },
-        { value: "Suurepärane", title: "Suhtlemisoskus" },
-      ]}
+      textboxLayout="split-description"
+      useInvertedBackground={false}
+      description="Siia saate sisestada pikema teksti oma teekonna kohta. Olen 18-aastane Tallinnast ning läbi aastate olen kogunud mitmekülgseid kogemusi tantsu, spordi ja fotograafia vallas. See tekstikast on nüüd piisavalt lai, et mahutada kogu teie lugu, kirjeldusi ja mõtteid, ilma et peaksite muretsema ruumipuuduse pärast. See võimaldab paremini esile tuua teie arengut ja huvisid, muutes teie portfoolio sisukamaks ja personaalsemaks."
       imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778674801513-jntq5j0h.jpg"
       mediaAnimation="slide-up"
-      metricsAnimation="slide-up"
       imageAlt="dance studio action shot"
     />
   </div>
@@ -71,14 +66,12 @@ export default function LandingPage() {
       textboxLayout="split"
       useInvertedBackground={false}
       title="Minu projektid"
-      description="Siin on ülevaade minu varasematest projektidest ja tegevustest."
+      description="Iseseisvad tekstikastid minu erinevatele tegevusvaldkondadele."
       features={[
-        { id: "p1", title: "Fc Mis Vennad ", description: "Rahvaliiga klubi loomine (Fc Mis Vennad).", tag: "Jalgpall", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778530873115-vm1ju69k.png" },
-        { id: "p2", title: "Võistlustants ", description: "Meeskonnatöö arendamine läbi spordi.", tag: "Tants", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778531466534-7mwwuifz.jpg" },
-        { id: "p3", title: "Klienditeenindus", description: "Probleemide lahendamine iga päaev ", tag: "Teenindus", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778671852101-8oq8hsvm.jpg" },
-        { id: "p4", title: "Meedia ja Disaini suund ", description: "Kiili Gümnaasiumi jooksul projektid ", tag: "Elu ", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778670506849-y65r1qaw.jpg?_wi=2" },
-        { id: "p5", title: "Fotograafia ", description: ".... ", tag: "Innovatsioon", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778671911732-kjp1j0l4.jpg?_wi=1" },
-        { id: "p6", title: "Reisimine ", description: "Tutvu koos minuga veidike riikidest ", tag: "Elu ", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778672237771-31zfv4kh.jpg" }
+        { id: "p1", title: "Fc Mis Vennad", description: "Rahvaliiga klubi loomine (Fc Mis Vennad).", tag: "Jalgpall", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778530873115-vm1ju69k.png" },
+        { id: "p2", title: "Võistlustants", description: "Meeskonnatöö arendamine läbi spordi.", tag: "Tants", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778531466534-7mwwuifz.jpg" },
+        { id: "p3", title: "Meedia ja Disain", description: "Kiili Gümnaasiumi jooksul läbi viidud loovprojektid.", tag: "Meedia", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778670506849-y65r1qaw.jpg?_wi=2" },
+        { id: "p4", title: "Reisimine", description: "Maailma avastamine ja uued kogemused erinevates riikides.", tag: "Reisimine", imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3DXy63pHOXTf1wMgW8x6XNMvtik/uploaded-1778672237771-31zfv4kh.jpg" }
       ]}
     />
   </div>
