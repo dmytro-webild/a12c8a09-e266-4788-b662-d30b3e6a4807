ea2a82e8237d0da1ad5a3d75f1fbef934f8a9c73
bender <kudin.dima@gmail.com>
2026-05-13 11:10:22 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,6 +92,7 @@ export default function LandingPage() {
       mediaAnimation="slide-up"
       mediaPosition="right"
       imageSrc="http://img.b2bpic.net/free-photo/portrait-stylish-handsome-young-man-looking-camera-leaning-grey-wall_23-2148130416.jpg?_wi=3"
+      termsText="By clicking Sign Up you're confirming that you agree with our Terms and Conditions."
     />
   </div>
 
