fc795c591338cbc4a93850c2bd47b723fbe7d0cf
bender <kudin.dima@gmail.com>
2026-05-15 17:19:25 +0000

    Update src/app/contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import ReactLenis from "lenis/react";
 import NavbarStyleFullscreen from '@/components/navbar/NavbarStyleFullscreen/NavbarStyleFullscreen';
 import FooterCard from '@/components/sections/footer/FooterCard';
 import ContactSplit from '@/components/sections/contact/ContactSplit';
-import { TikTok, Instagram } from 'lucide-react';
+import { Instagram, Zap } from 'lucide-react';
 
 export default function ContactPage() {
   return (
@@ -53,7 +53,7 @@ export default function ContactPage() {
           <FooterCard
             logoText="Lazar Uleksin"
             socialLinks={[
-                { icon: TikTok, href: "#", ariaLabel: "TikTok" },
+                { icon: Zap, href: "#", ariaLabel: "TikTok" },
                 { icon: Instagram, href: "https://www.instagram.com/lazaruleksin?igsh=dzh6bDl4emVyN2dj&utm_source=qr", ariaLabel: "Instagram" }
             ]}
           />
