acd60575fcfbf7e7b330eed4c20c266d0ee8273f
bender <kudin.dima@gmail.com>
2026-05-16 21:35:20 +0000

    Switch to version 116: modified src/app/about-page/page.tsx

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -29,7 +29,6 @@ export default function AboutPage() {
             navItems={[
               { name: "Home", id: "/" },
               { name: "Minu kohta", id: "/about-page" },
-              { name: "Tööde arhiiv", id: "/tööde-arhiiv" },
               { name: "Projektide detailne kirjeldus", id: "/projects" },
               { name: "Võta ühendust", id: "/contact" },
             ]}
