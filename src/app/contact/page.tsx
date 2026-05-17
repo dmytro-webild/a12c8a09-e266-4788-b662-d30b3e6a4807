deeacb3eb03379ccf0c6f83d7bd72099b46c860c
bender <kudin.dima@gmail.com>
2026-05-17 20:34:48 +0000

    Update src/app/contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,7 +28,7 @@ export default function ContactPage() {
               { name: "Home", id: "/" },
               { name: "Minu kohta", id: "/about-page" },
               { name: "Projektide detailne kirjeldus", id: "/projects" },
-              { name: "Tööde arhiiv", id: "/toode-arhiiv" },
+              { name: "Tööde arhiiv", id: "https://canva.link/ioqz9m5udeiwbjz" },
               { name: "Võta ühendust", id: "/contact" },
             ]}
             brandName="Lazar Uleksin"
